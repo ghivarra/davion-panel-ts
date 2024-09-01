9502ad4deb5587e0286afc03f1df552d79c9b7da
Ghivarra Senandika Rushdie <gsenandika@gmail.com>
2024-09-01 14:15:08 +0700

    update Common.php

--- app/Common.php
+++ app/Common.php
@@ -13,3 +13,52 @@
  *
  * @see: https://codeigniter.com/user_guide/extending/common.html
  */
+
+use CodeIgniter\HTTP\ResponseInterface;
+use Config\Services;
+
+function imageUrl($slug = '', $width = NULL, $height = NULL, $priority = 'width'): string
+{
+    $url = base_url("assets/images/{$slug}?priority={$priority}");
+
+    if (!is_null($width))
+    {
+        $url .= "&width={$width}";
+    }
+
+    if (!is_null($height))
+    {
+        $url .= "&height={$height}";
+    }
+
+    return $url;
+}
+
+function numbering(array $array = [], int $start = 0): array
+{
+    foreach ($array as $key => $value):
+
+        $start++;
+        $array[$key]['no'] = number_format($start, 0, ',', '.');
+
+    endforeach;
+
+    return $array;
+}
+
+function prettyPrint($var): void
+{
+    echo '<pre>';
+    var_dump($var);
+    echo '</pre>';
+}
+
+function cannotAccessModule($status = 'error', $message = 'Anda tidak memiliki izin untuk mengakses halaman ini.'): ResponseInterface
+{
+    $response = Services::response();
+
+    return $response->setStatusCode(403)->setJSON([
+        'status'  => $status,
+        'message' => $message
+    ]);
+}
