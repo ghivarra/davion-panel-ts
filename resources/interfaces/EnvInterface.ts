39a712d8a00d0b892550cc3898e7012cd98b9b1b
Ghivarra Senandika Rushdie <gsenandika@gmail.com>
2024-09-02 15:53:10 +0700

    create env interfaces

---
+++ resources/interfaces/EnvInterface.ts
@@ -0,0 +1,23 @@
+interface EnvInterface {
+    BASE_URL?: string,
+    DEV?: boolean,
+    MODE?: string,
+    PROD?: boolean,
+    SSR?: boolean,
+    VITE_NODE_ENV?: string,
+    VITE_URL?: string
+    VITE_APP_ENV?: string
+    VITE_APP_NAME?: string
+    VITE_APP_ID?: string
+    VITE_ORIGIN?: string
+    VITE_PORT?: string
+    VITE_ASSETS_DIR?: string
+    VITE_ENTRY_FILE?: string
+    VITE_RESOURCES_DIR?: string
+    VITE_LOGIN_PAGE?: string
+    VITE_PANEL_PAGE?: string
+    VITE_USE_LOADER?: string
+    VITE_LOADER_ON_CHANGE_PAGE?: string
+}
+
+export type { EnvInterface }
