1d6c541ed07311ce7501b662c20cdb64b2dfe449
Ghivarra Senandika Rushdie <gsenandika@gmail.com>
2024-09-04 14:00:29 +0700

    done router for login

--- resources/router/index.ts
+++ resources/router/index.ts
@@ -1,28 +1,15 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import type { RouteRecordRaw } from 'vue-router'
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/login',
-      name: 'login',
-      component: HomeView
-    },
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+interface RouterIndexInterface {
+	default: RouteRecordRaw[]
+}
+
+const router = (): Promise<RouterIndexInterface> => {
+	// uncomment this if using chunked files
+	// return import('@/router/config/index-chunked')
+
+	// uncomment this if using single files
+	return import('@/router/config/index-single')
+}
 
 export default router
