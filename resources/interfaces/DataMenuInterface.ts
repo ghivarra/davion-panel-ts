2d889c5d6e966d8d4fd56e4f1f3e71bf42bfcfd2
Ghivarra Senandika Rushdie <gsenandika@gmail.com>
2024-09-05 19:40:23 +0700

    update menu interfaces

--- resources/interfaces/DataMenuInterface.ts
+++ resources/interfaces/DataMenuInterface.ts
@@ -2,7 +2,8 @@ interface ChildMenuInterface {
     id: string | number,
     title: string,
     router_name: string,
-    parent_id: string | number
+    parent_id: string | number,
+    is_active?: boolean ,
 }
 
 interface MenuInterface {
@@ -13,11 +14,14 @@ interface MenuInterface {
     type: string,
     admin_menu_group_id: string | number,
     admin_menu_group_name: string,
-    childs?: ChildMenuInterface[]
+    childs?: ChildMenuInterface[],
+    is_active?: boolean,
 }
 
-export interface DataMenuInterface {
+interface GroupMenuInterface {
     id: string | number,
     name: string,
     menu: MenuInterface[],
 }
+
+export type { GroupMenuInterface, MenuInterface, ChildMenuInterface }
