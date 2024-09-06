4ec3cd289c86b9ffdb74f207d9148afd10bec79b
Ghivarra Senandika Rushdie <gsenandika@gmail.com>
2024-09-06 18:49:53 +0700

    add datatable interfaces

---
+++ resources/interfaces/Datatable/DatatableAdminInterface.ts
@@ -0,0 +1,15 @@
+export interface DatatableAdminInterface {
+    id: number | string,
+    username: string,
+    name: string,
+    email: string,
+    email_verified_at: string,
+    status: string,
+    admin_role_id: number | string,
+    admin_role_name: string,
+    is_superadmin: number | string,
+    photo: string | null,
+    statusDefault?: string,
+    superadminDefault?: string | number,
+    action?: string,
+}
